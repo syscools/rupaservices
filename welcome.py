c4ed073e480e0d6bf8b22a0286adece3e960b38e
dexter aparicio <syscool@gmail.com>
2016-11-19 12:58:11 +0000

    rendering a google map of a lat,lon

--- welcome.py
+++ welcome.py
@@ -21,12 +21,58 @@ from flask import Response
 import json
 
 API_KEY = 'AIzaSyCzpbgoECGhplawnFLBNgCVStWyMY30Ku8'
+HTML="""
+<html>
+<iframe width="450" height="250" frameborder="0" style="border:0" 
+    src="https://www.google.com/maps/embed/v1/streetview?key=AIzaSyCzpbgoECGhplawnFLBNgCVStWyMY30Ku8&location={lat},{lon}&heading=210&pitch=10&fov=35" allowfullscreen> </iframe>
+</html>
+"""
+
+HTML2="""
+<iframe width="450" height="250" frameborder="0" style="border:0" 
+    src="https://www.google.com/maps/embed/v1/streetview?key=AIzaSyCzpbgoECGhplawnFLBNgCVStWyMY30Ku8&location={lat},{lon}&heading=210&pitch=10&fov=35" allowfullscreen> </iframe>
+"""
+
+HTML3="{lat},{lon}"
 
 app = Flask(__name__)
 
 @app.route('/')
 def Welcome():
     return app.send_static_file('index.html')
+
+@app.route('/places2')
+def places2():
+    location = (10.3099,123.893)
+    val = request.args.get('q')
+    if val:
+        try:
+            gmaps = googlemaps.Client(key = API_KEY)
+            loc = gmaps.places(val,location = location,radius = 100);
+        except:
+            resp = Response("{'addresses':[],'timeout':'yes'}")
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            return resp
+       
+        def get_addresses(b): # a is a string
+            c = b["results"]
+            e = []
+            for x in c:
+                # convert dict to string using json.dumps()
+                d = json.dumps(x["formatted_address"])
+                lon = json.dumps(x["geometry"]["location"]["lng"])
+                lat = json.dumps(x["geometry"]["location"]["lat"])
+                html = HTML3.format(lon=lon,lat=lat)
+                if len(d) > 2: # exclude empty stuff
+                    address = x["formatted_address"]
+                    node = {'address':address,'latlon':html}
+                    e.append(node)
+            return json.dumps({"addresses":e})
+
+        resp = Response(get_addresses(loc))
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
+    return redirect("/")
 
 @app.route('/places')
 def places():
